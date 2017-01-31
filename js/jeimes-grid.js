04a10aa23705e354f2110ee2897c930766321eba
Wilson Prado <wilson@dcide.com.br>
2017-01-31 10:18:40 -0200

    ajustes iniciais

--- js/jeimes-grid.js
+++ js/jeimes-grid.js
@@ -1,9 +1,11 @@
+var app = angular.module("jeimes",[]);
+
 app.directive("jeimesGrid",function(){
     return {
         restrict: "A",
         scope: {},
         link: function(scope,elem,attr){
-
+            console.log(scope);
         }
     }
 });
